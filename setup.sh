b9c009bb8d586432162468d585444a8b89c8316f
Bohdan Khaletskyi <bohdan.khaletckyi@gmail.com>
2026-01-16 13:31:12 +0200

    update help command

--- setup.sh
+++ setup.sh
@@ -18,8 +18,20 @@ ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+if command -v zsh >/dev/null 2>&1; then
+	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+else
+	echo "zsh not found (skipping oh-my-zsh install)"
+fi
 
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
-brew install stats bash-language-server yaml-language-server ripgrep neovim kitty
+OS_NAME="$(uname -s)"
+if [ "$OS_NAME" = "Darwin" ]; then
+	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+	brew install stats bash-language-server yaml-language-server ripgrep neovim kitty
+elif [ -f /etc/os-release ] && \
+	( grep -q '^ID=debian' /etc/os-release || grep -q '^ID=ubuntu' /etc/os-release ); then
+	sudo apt-get update
+	sudo apt-get install -y ripgrep neovim kitty bash-language-server node-yaml-language-server
+else
+	echo "Unsupported OS for package install (skipping)."
+fi
